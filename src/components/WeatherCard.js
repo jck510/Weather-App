b33fdefda4f93d0416fc75c3d1dd97b269f800c9
Joshua Knudsen <knudsen.joshua772@gmail.com>
2021-12-14 16:00:55 -0800

    Minor tags added to WeatherCard

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -3,7 +3,8 @@ import React from 'react'
 const WeatherCard = () => {
     return (
         <div>
-            
+            <h3>Location</h3>
+            <h4>Temperature</h4>
         </div>
     )
 }
